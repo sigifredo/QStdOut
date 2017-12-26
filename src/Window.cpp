621887c514ac36a3677f2248b3bdd5ee53ab0de5
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2017-12-26 16:30:16 -0500

    Conectamos los objetos y probamos que funcione la estructura.

--- src/Window.cpp
+++ src/Window.cpp
@@ -7,19 +7,24 @@
 
 // Qt
 #include <QBoxLayout>
-#include <QTextEdit>
+#include <QPlainTextEdit>
 
 Window::Window(QWidget * pParent):
     QWidget(pParent)
 {
     QBoxLayout * pLayout = new QBoxLayout(QBoxLayout::LeftToRight, this);
-    QTextEdit * pTextEdit = new QTextEdit(this);
+    QPlainTextEdit * pTextEdit = new QPlainTextEdit(this);
     QStreamListener * pStreamListener = new QStreamListener(this);
     _pCustomStream = new CustomStream(std::cout, pStreamListener);
 
-    connect(pStreamListener, SIGNAL(textCatched(const QString&)), pTextEdit, SLOT(append(const QString&)));
+    connect(pStreamListener, SIGNAL(textCatched(const QString&)), pTextEdit, SLOT(insertPlainText(const QString&)));
 
     pLayout->addWidget(pTextEdit);
+
+    std::cout << "prueba2";
+    std::cout << "prueba3" << std::endl;
+    std::cout << std::endl;
+    std::cout << "prueba4" << std::endl;
 }
 
 Window::~Window()
